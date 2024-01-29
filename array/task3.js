b439dfecb76badb3fddcf826db0ac37678f99b3d
biswadebraj7 <biswadebraj@gmail.com>
2024-01-29 20:43:58 +0600

    task complete

--- array/task3.js
+++ array/task3.js
@@ -6,3 +6,14 @@ Create an array of books containing different book.
 Use the includes method to check if the array contains a javascript book.
 
 Print a message to the console indicating whether the element is present in the array or not. */
+
+const book1=["javabook","c-book","C++book","python-book","javascript"];
+
+
+if(book1.includes('javascript')){
+    console.log("Here are include javascript book");
+    console.log(book1)
+
+}else{
+    console.log(book1);
+}
--- array/task4.js
+++ array/task4.js
@@ -6,3 +6,10 @@ Create different variables, each containing either an array or a non-array value
 Now use isArray to check if each variable is an array.
 
 Print a message to the console indicating whether each variable is an array or not. */
+
+const list=["Hi","Hello",5,6,7,97,"how","what"];
+if(Array.isArray(list)){
+    console.log("this is array")
+}else{
+    console.log("not arraya")
+}
